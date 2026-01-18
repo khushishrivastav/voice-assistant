9ce4b89c42285794028b6681568a7b5a332799e7
khushishrivastav <shrivastavKhushi488@gmail.com>
2026-01-18 15:09:08 +0530

    Update script.js

--- Front-End/script.js
+++ Front-End/script.js
@@ -5,6 +5,7 @@ recognition.lang="en-US";
  
 let btn= document.getElementById("btn");
 let voice=document.getElementById("voice");
+let wished=false;
 btn.addEventListener("click",()=>
 {function speak(text) {
   const abc = new SpeechSynthesisUtterance(text);
@@ -19,13 +20,40 @@ btn.addEventListener("click",()=>
 
       function handleCommands(command)
       {
-       btn.style.display="flex";
+       btn.style.display="flex"; 
        voice.style.display="none";
-        if(command.includes("how r u ?"))
-        { speak("i'm fine ..what about uh ?");}
-        if(command.includes("what r u doing"))
+
+         if(command.includes("who are you")||command.includes("who r u ")||command.includes("hu r u "))
+{ speak("i'm your virtual assistant");}
+
+   else if(command.includes("how are you")||command.includes("how r u "))
+{ speak("i'm good , what about you ");}
+
+      else if(command.includes("what you can do")||command.includes("what u can do "))
+{speak("i can open apps and do can do any task");}
+
+       else if(command.includes("who developed you")||command.includes("who made you"))
+{ speak("miss khushi shrivastav"); }
+
+else if(command.includes("open browser")||command.includes("open chrome")||command.includes("open google"))
+        {
+            speak("opening google");
+            window.open("https://www.google.com/");
+        }
+else if(command.includes("date"))
+{ let date= new Date().toLocaleDateString();
+  speak(date);
+}
+else if(command.includes("time"))
+{ let time= new Date().toLocaleTimeString();
+ 
+  speak(time);
+}
+
+
+      else  if(command.includes("what are you doing")||command.includes("what r u doing "))
         { speak("i'm listening to u");}
-        if(command.includes("open youtube"))
+     else if(command.includes("open youtube"))
         {
             speak("opening youtube");
             window.open("https://www.youtube.com");
@@ -35,14 +63,14 @@ btn.addEventListener("click",()=>
             speak("opening facebook");
             window.open("https://www.facebook.com");
         }
-        else if(command.includes("open Instagram"))
+        else if(command.includes("open instagram"))
         {
-            speak("opening Instagram");
+            speak("opening instagram");
             window.open("https://www.instagram.com");
         }
          else if(command.includes("open whatsapp"))
         {
-            speak("opening Instagram");
+            speak("opening whatsapp");
             window.open("https://www.whatsapp.com");
         } 
 
@@ -50,9 +78,9 @@ btn.addEventListener("click",()=>
         {
           speak("opening notepad");
            setTimeout(()=>{ alert(" wait notepad is opening");}, 2000);
-          fetch("http://localhost:3000/open-notepad");
-          
+         fetch("http://localhost:3000/open-notepad");
         }
+
         else if(command.includes("open paint"))  
         {
           speak("opening mspaint");
@@ -104,8 +132,30 @@ btn.addEventListener("click",()=>
        { speak(`this is what i found on internet regarding ${command}`);
          window.open(`https://www.google.com/search?q=${command}`);
 } }
+    
+      if(!wished)
+{
+      let day= new Date();
+      let hours= day.getHours();
+     if(hours>=0 && hours<12)
+       {
+        speak("good morning , how can i help you");
+       }
+     else if(hours>=12 && hours<17)
+       {
+        speak("good afternoon , how can i help you");
+       }
+    else if(hours>=17 && hours<=19)
+       {
+        speak("good evening, How can i help you ");
+       }
+     else if(hours>19 && hours<=24)
+       {
+        speak("good evening, How can i help you ");
+       }
+       wished= true;
       
-      speak("how can i help you");
+}
       setTimeout(() => {
         recognition.start();
          btn.style.display="none";
