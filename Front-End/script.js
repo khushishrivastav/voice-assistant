4a0bee425f99fc54d45bbc636ddbd7747eea1907
khushishrivastav <shrivastavKhushi488@gmail.com>
2026-01-24 16:21:17 +0530

    Update script.js

--- Front-End/script.js
+++ Front-End/script.js
@@ -1,14 +1,13 @@
 const recognition = new(window.SpeechRecognition || window.webkitSpeechRecognition)();
-//start listening convert voice into text
+//start listening user voice and  convert user voice into text
 recognition.lang="en-US";
-//const x = require('child_process');
  
 let btn= document.getElementById("btn");
 let voice=document.getElementById("voice");
 let wished=false;
 btn.addEventListener("click",()=>
 {function speak(text) {
-  const abc = new SpeechSynthesisUtterance(text);
+  const abc = new SpeechSynthesisUtterance(text); //convert text to speech and voice
 
   abc.lang="hi-GB";
   abc.volume=1;
@@ -44,6 +43,7 @@ else if(command.includes("date"))
 { let date= new Date().toLocaleDateString();
   speak(date);
 }
+
 else if(command.includes("time"))
 { let time= new Date().toLocaleTimeString();
  
@@ -51,7 +51,7 @@ else if(command.includes("time"))
 }
 
 
-      else  if(command.includes("what are you doing")||command.includes("what r u doing "))
+      else if(command.includes("what are you doing")||command.includes("what r u doing "))
         { speak("i'm listening to u");}
      else if(command.includes("open youtube"))
         {
@@ -170,7 +170,7 @@ recognition.onresult = (event) => {
 }
 );
 
-let speechtotext= document.getElementById("speechtotext");
+/*let speechtotext= document.getElementById("speechtotext");
 let btn1= document.getElementById("btn1");
 btn1.addEventListener("click", ()=>{
 
@@ -190,4 +190,164 @@ btn1.addEventListener("click", ()=>{
   setTimeout(
     () => {cleartext.style.display="none";}
     , 2000);
-});
+}); */
+
+ var aichatarea=document.getElementById("aichatarea");
+let user= {data :null};
+
+const api_url="https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent" ;
+const a1= "AIzaSyA3VZ";
+const b2= "-EsZr6tBAvmtdbFv6";
+const c3=" upPlZad75tJc";
+
+function generate()
+{ return a1+b2+c3;}
+
+key=generate();
+
+async function generateResponse(aichatbox)
+{  
+
+ 
+  let requestOption= { method : "POST" ,
+                      
+                  headers: {
+      "Content-Type": "application/json",
+      "x-goog-api-key": key
+    },
+    body: JSON.stringify({
+      contents: [
+        {
+          parts: [
+            {
+              text:aichatbox
+            }
+          ]
+        }
+      ]
+    })
+  };
+ 
+
+  try {
+  
+    let response = await fetch(api_url, requestOption);
+    
+    let data = await response.json();
+    
+  console.log("full response" , data);
+   if (!data.candidates) {
+      return "⚠️ API limit reached,try again after sometime.";
+    }
+
+   let aitext= data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g,"$1").trim(); 
+ return aitext;
+}
+
+ catch(error) {
+    console.error("Error:", error);
+  }
+
+}
+
+ 
+
+var chatcont=document.getElementById("chat-container");
+  var userchatarea=document.getElementById("usr-chatarea");
+  var parentuserchatarea=document.getElementById("user-chatarea"); 
+  var aichatarea=document.getElementById("aichatareaa");
+  var usrbtn1= document.getElementById("user-btn1");
+
+   var text=document.getElementById("prompt").value;
+  var value=true;
+  
+
+  
+  usrbtn1.addEventListener("click",async()=>
+ {   //chatcont.style.backgroundColor="rgba(0,0,0,0.8)";
+    usrbtn1.disabled = true;
+   setTimeout(()=>{ usrbtn1.disabled = false; },8000);
+
+
+ /*  function button()
+{ if(text==="")
+{usrbtn1.disabled=true;}
+else
+{ usrbtn1.disabled=false;} }
+
+button(); */
+
+ var text= document.getElementById("prompt").value;
+ if(text=="")
+{return ;}
+ user.data = text;
+ let aireply= await generateResponse(text);
+
+ if(value)
+  { chatcont.style.display="block";
+   userchatarea.innerText= text;
+   document.getElementById("prompt").value="";
+  usrbtn1.disabled=true; 
+  aichatarea.innerText=aireply;
+     usrbtn1.disabled=false;
+
+  value=false;
+  }
+ else
+
+{
+    var img1= document.createElement("img");
+   img1.src="user.png";
+   img1.style.width="30px";
+ 
+
+  var div= document.createElement("div");
+  var textnode=document.createTextNode(text);
+  div.appendChild(img1);
+  div.appendChild(textnode);
+  chatcont.appendChild(div);
+  chatcont.style.display="block";
+  document.getElementById("prompt").value=""; 
+   div.style.backgroundColor= "black";
+//div.style.backgroundColor= "#87ceeb"
+  div.style.color= "white";
+  div.style.marginLeft=  "auto";
+  div.style.fontSize= "20px";
+  div.style.width= "50%";
+  div.style.borderRadius= "20px";
+  div.style.wordWrap=  "break-word";
+  div.style.gap="20px";
+   //value=false;
+ 
+    var img2= document.createElement("img");
+   img2.src="ai.image.png";
+   
+   img2.style.width="30px";
+   
+   usrbtn1.disabled=true;
+   var div1= document.createElement("div");
+  var textnode1=document.createTextNode(aireply);
+  div1.appendChild(img2);
+  div1.appendChild(textnode1);
+  chatcont.appendChild(div1);
+   usrbtn1.disabled=false;
+  chatcont.style.display="block";
+ // alert(aireply);
+  
+   div1.style.backgroundColor= "rgba(0,0,0,0.20)";
+   // div1.style.backgroundColor= "#ffb6c1";
+    div1.style.color= "white";
+    div1.style.display= "flex";
+   div1.style.marginRight="auto";
+   div1.style.fontSize="20px";
+   div1.style.borderRadius="20px";
+   div1.style.width="50%";
+   div1.style.gap="20px";
+   
+    
+
+}
+
+   });
+
+
